6e9852b94b222d763571e6260142780b011e0713
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-12-09 22:20:32 +0100

    Refactoring

--- visualize_gcs.py
+++ visualize_gcs.py
@@ -15,28 +15,16 @@ tags = list(filter(lambda tag:
     tag != '0.1-incubating' and 
     tag != 'tika-1.3', tags))
 tags.sort(key=parse_version) # Sort by semantic versioning
+
+# Compute # God Components per tag
 n_godcomps = []
 for tag in tags:
     godcomps = data[data['Tag'] == tag].groupby('Package Name')
     n_godcomps.append(len(godcomps))
+
+# Make line plot
 df = pd.DataFrame({'# God Components': n_godcomps, 'Tag': tags})
 sns.lineplot(data=df, x='Tag', y='# God Components', sort=False)
-
-# # Prepare data
-# grouped = data.groupby(['Package Name', 'Tag']).count()
-# grouped = grouped.sort_values(['Package Name', 'Tag'])
-# grouped = grouped.diff()
-# grouped['Commits made'] = grouped['CommitID'].replace(np.nan, 0).astype(int)
-# grouped = grouped.reset_index()
-
-# # Heatmap
-# rectangular = pd.pivot_table(grouped,\
-#     values='Commits made',\
-#     index=['Package Name'],\
-#     columns=['Tag'])
-# g = sns.heatmap(rectangular,\
-#     annot=True, linewidths=.5,\
-#     cmap="YlGnBu", yticklabels=True)
 
 # Rotate labels
 locs, labels = plt.xticks()
