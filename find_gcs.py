ffc52e2e1cf7270091196b6171075f9c3a401a10
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-12-12 16:20:29 +0100

    Parallel processing & commit ids 🚀 Instead of tags

--- find_gcs.py
+++ find_gcs.py
@@ -2,11 +2,12 @@ import os
 from time import time
 import pandas as pd
 import subprocess
+import multiprocessing
 
 # Settings
 KEY = os.environ.get('DESIGNITE_ENTERPRISE')
 RUNTIME_JAR = 'designite/DesigniteJava_Enterprise.jar'
-TIKA_REPO = '../tika'
+REPOSITORIES = 'designite/repositories'
 OUTPUT_FOLDER = 'designite/reports'
 # Vars
 jar = 'java -jar {}'.format(RUNTIME_JAR)
@@ -18,10 +19,10 @@ if (not os.path.exists('designite/.config')):
     os.system('{} -r {}'.format(jar, KEY))
 
 # Run Designite on the currently checked out tag
-def designite(output_folder):
+def designite(output_folder, repo):
     # Run Designite
     start = time()
-    os.system('{} -i {} -o {}'.format(jar, TIKA_REPO, output_folder))
+    os.system('{} -i {} -o {}'.format(jar, repo, output_folder))
     print('Designite ran for {:.2f} seconds'.format(time() - start))
 
     # Filter results for God Components
@@ -31,21 +32,51 @@ def designite(output_folder):
     os.system('rm -rf {}'.format(output_folder)) # Remove report
     return godcomps
 
-# Grab tags
-tags = subprocess.check_output(['git', 'tag', '-l'],
-    cwd=TIKA_REPO, encoding='utf-8').splitlines()
-for tag in tags:
-    targetfile = '{}/{}.csv'.format(OUTPUT_FOLDER, tag)
+def run_designite(commit_id):
+    targetfile = '{}/{}.csv'.format(OUTPUT_FOLDER, commit_id)
+    cpu, _ = multiprocessing.Process()._identity
     if (os.path.exists(targetfile)):
-        print('Skipping tag', tag)
-        continue
-    print('Running Designite for tag', tag)
-    os.system('git checkout ' + tag)
-    godcomps = designite('{}/{}'.format(OUTPUT_FOLDER, tag))
-    godcomps['Tag'] = tag
+        print('Skipping {} [cpu #{}]'.format(commit_id, cpu))
+        return
+    print('Running Designite for {} [cpu #{}]'.format(commit_id, cpu))
+    clone_tika(cpu)
+    subprocess.run(['git', 'checkout', commit_id], cwd=repo(cpu))
+    godcomps = designite('{}/{}'.format(OUTPUT_FOLDER, commit_id), repo(cpu))
+    godcomps['Commit ID'] = commit_id
     godcomps.to_csv(targetfile, index=False)
 
-# Combine all reports into 1 .csv file
-paths = map(lambda f: os.path.join(OUTPUT_FOLDER, f), os.listdir(OUTPUT_FOLDER))
-all_reports = pd.concat(map(lambda f: pd.read_csv(f, dtype=str), paths))
-all_reports.to_csv('designite/all_reports.csv', index=False)
+def repo(cpu):
+    return '{}/tika-cpu_{}'.format(REPOSITORIES, cpu)
+
+def clone_tika(cpu):
+    if (os.path.exists(repo(cpu))): return
+    subprocess.run(['git', 'clone', 'https://github.com/apache/tika.git',
+        'tika-cpu_{}'.format(cpu)], cwd=REPOSITORIES)
+
+# Grab tags and run Designite
+if __name__ == '__main__':
+    cpus = int(os.environ.get('SLURM_JOB_CPUS_PER_NODE', \
+                            multiprocessing.cpu_count()))
+    cpus = 2
+    if (not os.path.exists(REPOSITORIES)): os.makedirs(REPOSITORIES)
+
+    # list all available tags on cpu_1 repo
+    clone_tika(1)
+    # tags = subprocess.check_output(['git', 'tag', '-l'],
+    #     cwd='{}/tika-cpu_1'.format(REPOSITORIES), encoding='utf-8').splitlines()
+    subprocess.run(['git', 'checkout', 'main'], cwd=repo(1))
+    subprocess.run(['git', 'pull'], cwd=repo(1))
+    commit_ids = subprocess.check_output(['git', 'log', '--pretty=format:%h'],
+        cwd=repo(1), encoding='utf-8').splitlines()
+    print('Using {} cores.'.format(cpus))
+
+    # Run Designite on all cores
+    pool = multiprocessing.Pool(processes=cpus)
+    pool.map(run_designite, commit_ids)
+    pool.close()
+    pool.join()
+
+    # Combine all reports into 1 .csv file
+    paths = map(lambda f: os.path.join(OUTPUT_FOLDER, f), os.listdir(OUTPUT_FOLDER))
+    all_reports = pd.concat(map(lambda f: pd.read_csv(f, dtype=str), paths))
+    all_reports.to_csv('designite/all_reports.csv', index=False)
