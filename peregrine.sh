b830c758a33629ec39a0dcc52d168eb68ca86253
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-12-13 09:53:28 +0100

    Add sbatch script

---
+++ peregrine.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --time=70:00:00
+#SBATCH --mem=128000
+#SBATCH --nodes=1
+#SBATCH --partition=regular
+#SBATCH --job-name=sme_god-components
+
+module load Python/3.6.4-foss-2018a
+pip3 install -r requirements.txt --user
+python3 find_gcs.py
