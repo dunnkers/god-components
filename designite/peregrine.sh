bfe68ed227ac56bf3925fcdaa6d28533cd286e62
dunnkers <jeroenoverschie@gmail.com>
2020-12-15 10:12:23 +0100

    High-memory job

--- designite/peregrine.sh
+++ designite/peregrine.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --time=70:00:00
-#SBATCH --mem=128000
+#SBATCH --mem=1024000
 #SBATCH --nodes=1
 #SBATCH --ntasks=24
-#SBATCH --partition=regular
+#SBATCH --partition=himem
 #SBATCH --job-name=sme_god-components
-#SBATCH --output=designite/logs/slurm-%j.out#!/bin/bash
+#SBATCH --output=designite/logs/slurm-%j.out
 
 module load Python/3.6.4-foss-2018a
 pip3 install -r designite/requirements.txt --user
