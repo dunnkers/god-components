fdb563d11203708982dd56bb6702a0c712c9dcf1
Jeroen Overschie <jeroenoverschie@gmail.com>
2020-12-15 14:47:05 +0100

    Add top-5 commits

--- designite/chart-gcs_versus_locs.py
+++ designite/chart-gcs_versus_locs.py
@@ -20,6 +20,16 @@ for godcomp, df in all_locs.groupby('godcomp'):
     godcomp_dfs.append(df)
 data = pd.concat(godcomp_dfs)
 
+# Commits that most contributed to deletions/additions
+selected = []
+for godcomp, df in data.groupby('godcomp'):
+    # Commits that most contributed to deletions/additions
+    selected.append(df.sort_values('change', ascending=True)[:5].copy())
+    selected.append(df.sort_values('change', ascending=False)[:5].copy())
+    # ^ e.g. top 5 commits
+pd.concat(selected).to_csv('designite/output/top_5_commits-gc-buildup.csv',
+    index=False)
+
 # Plot
 fig, ax = plt.subplots()
 ax.set(yscale="log")
