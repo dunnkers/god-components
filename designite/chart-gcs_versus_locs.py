e7d85c3ceec0ae934360aa0360dcbdcb07d688a7
dunnkers <jeroenoverschie@gmail.com>
2020-12-15 13:01:33 +0100

    Better plot

--- designite/chart-gcs_versus_locs.py
+++ designite/chart-gcs_versus_locs.py
@@ -8,16 +8,23 @@ import numpy as np
 all_locs = pd.read_csv('designite/output/all_locs.csv', dtype={
     'additions': 'float',
     'deletions': 'float'
-})
+}, parse_dates=['datetime'])
+
+# Compute LOC change for every godcomp
+godcomp_dfs = []
 for godcomp, df in all_locs.groupby('godcomp'):
-        
+    print(godcomp, '...')
     df = df.sort_values('datetime')
     df['change'] = df['additions'] - df['deletions']
     df['LOC'] = df['change'].cumsum()
-    sns.lineplot(data=df, x='datetime', y='LOC', hue='godcomp')
-    plt.show()
-    print('end')
-    break
-# sns.lineplot(data=data, x='datetime', y='metric', hue='package')
-# plt.show()
+    godcomp_dfs.append(df)
+data = pd.concat(godcomp_dfs)
+
+# Plot
+fig, ax = plt.subplots()
+ax.set(yscale="log")
+palette = sns.color_palette('tab10', len(godcomp_dfs))
+sns.lineplot(data=data, x='datetime', y='LOC', hue='godcomp',
+                ax=ax, palette=palette)
+plt.show()
 print('end')
