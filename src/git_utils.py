7cb66202cdf5a685f96913b0d6f2b5ce6fd6c3cd
dunnkers <jeroenoverschie@gmail.com>
2021-01-15 13:59:27 +0100

    Refactor statistics 💠 Better I/O and cleaner Notebook

--- src/git_utils.py
+++ src/git_utils.py
@@ -27,9 +27,6 @@ def git_checkout(cpu, commit_id):
 
 # Return array of commits by using `git log` command
 def get_commits():
-    file = 'output/all_commits.csv'
-    if (os.path.exists(file)):
-        return pd.read_csv(file, parse_dates=['datetime'])
     git_checkout(1, 'main')
     subprocess.run(['git', 'pull'], cwd=repo(1))
     commit_ids = subprocess.check_output(['git', 'log', 
@@ -40,7 +37,6 @@ def get_commits():
         'commit', 'author', 'email', 'datetime', 'message'
     ], parse_dates=['datetime'])
     commits['jira'] = commits['message'].str.extract('(TIKA-[0-9]{1,})')
-    commits.to_csv(file, index=False)
     return commits
 
 # Get commit # additions and # deletions using `git diff`
@@ -64,7 +60,7 @@ def get_commit_locs(commit_id):
     df['deletions'] = pd.to_numeric(df['deletions'])
     return df
 
-def compute_locs(godcomps, commit):
+def compute_locs(godcomps, commit_id):
     # sort God Components by their nesting level, based on dots (.)
     # in this way the algorithm will check inclusion of more nested GC's first,
     # i.e. `    'org.apache.tika.parser.microsoft` will match before',
@@ -79,11 +75,11 @@ def compute_locs(godcomps, commit):
 
     # loop god components, only assign 1 file path to 1 GC at maximum.
     results = [] # result of what god components this commit affected
-    locdf = get_commit_locs(commit.id)
+    locdf = get_commit_locs(commit_id)
     all_files = locdf['file'].copy()
-    for _, godcomp in godcompsdf.iterrows():
+    for package in godcompsdf['package'].values:
         # find files that affected this GC
-        path = godcomp['package'].replace('.', '/')
+        path = 'src/main/java/{}'.format(package.replace('.', '/'))
         files = all_files.str.contains(path) # affected files
         if not files.any(): continue
 
@@ -93,28 +89,19 @@ def compute_locs(godcomps, commit):
         # compute total Lines Of Code changed in all files affecting this GC
         total = locdf[files].sum() # total LOC added/deleted
         total = total.drop(labels='file')
-        total['package'] = godcomp['package']
-        result = pd.concat([total, commit])
-        results.append(result)
+        total['package'] = package
+        total['commit'] = commit_id
+        results.append(total)
     new_df = pd.DataFrame(results)
     return new_df
 
-def get_locs():
-    file = 'output/all_locs.csv'
-    if (os.path.exists(file)):
-        return pd.read_csv(file, \
-            usecols=['commit', 'additions', 'deletions', 'change', 'LOC'])
-    # read data
-    commits = get_commits()
-    assert(os.path.exists('output/all_reports.csv'))
-    all_reports = pd.read_csv('output/all_reports.csv')
-
+def get_locs(all_commits, all_reports):
     # Compute LOC's for every commit
     git_checkout(1, 'main') # make sure repo(1) is recently `git pull`'ed
     godcomps = all_reports['package'].unique()
     mapped_commits = []
-    for _, row in tqdm(commits.iterrows(), total=commits.shape[0]):
-        mapped_commits.append(compute_locs(godcomps, row))
+    for _, commit in tqdm(all_commits.iterrows(), total=all_commits.shape[0]):
+        mapped_commits.append(compute_locs(godcomps, commit.commit))
     locdata = pd.concat(mapped_commits)
 
     # Compute LOC change for every godcomp
@@ -125,5 +112,4 @@ def get_locs():
         df['LOC'] = df['change'].cumsum()
         godcomp_dfs.append(df)
     all_locs = pd.concat(godcomp_dfs)
-    all_locs.to_csv('output/all_locs.csv', index=False)
     return all_locs
--- src/git_utils_test.py
+++ src/git_utils_test.py
@@ -1,15 +1,34 @@
 import unittest
-from git_utils import get_commits
+from git_utils import get_commits, get_locs
 
 class TestSum(unittest.TestCase):
     def test_commits(self):
-        commits = get_commits()
-        self.assertTrue(len(commits) > 0)
-        self.assertTrue('author' in commits.columns)
-        self.assertTrue('email' in commits.columns)
-        self.assertTrue('datetime' in commits.columns)
-        self.assertTrue('message' in commits.columns)
-        self.assertTrue('jira' in commits.columns)
+        data = get_commits()
+        self.assertTrue(len(data) > 0)
+        self.assertTrue('author' in data.columns)
+        self.assertTrue('email' in data.columns)
+        self.assertTrue('datetime' in data.columns)
+        self.assertTrue('message' in data.columns)
+        self.assertTrue('jira' in data.columns)
+        data.to_csv('output/all_commits.csv', index=False)
+
+    def test_get_locs(self):
+        # load commits and reports
+        self.assertTrue(os.path.exists('output/all_commits.csv'))
+        self.assertTrue(os.path.exists('output/all_reports.csv'))
+        all_commits = pd.read_csv('output/all_commits.csv')
+        all_reports = pd.read_csv('output/all_reports.csv')
+
+        # compute LOC's
+        data = get_locs(all_commits, all_reports)
+        self.assertTrue(len(data) > 0)
+        self.assertTrue('commit' in data.columns)
+        self.assertTrue('package' in data.columns)
+        self.assertTrue('additions' in data.columns)
+        self.assertTrue('deletions' in data.columns)
+        self.assertTrue('LOC' in data.columns)
+        self.assertTrue('change' in data.columns)
+        data.to_csv('output/all_locs.csv', index=False)
 
 if __name__ == '__main__':
     unittest.main()
