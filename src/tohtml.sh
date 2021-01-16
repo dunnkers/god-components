0ff02c3e7804546bd1dac00455f07871d47f91a0
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-01-16 19:18:52 +0100

    Add script to update Notebook web version

---
+++ src/tohtml.sh
@@ -0,0 +1,6 @@
+cd ~/git/nbconvert-blog-template/
+jupyter nbconvert ~/git/sme_god-components/statistics.ipynb --to html --output-dir ~/git/sme_god-components_gh-pages --output index.html --template blog
+cd ~/git/sme_god-components_gh-pages
+git add .
+git commit -m "Update web version of Notebook at $( date +%T )"
+git push
