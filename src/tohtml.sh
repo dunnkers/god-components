9530f4c0edd5bd98a09b99b9f40ed0fa09566a54
Jeroen Overschie <jeroenoverschie@gmail.com>
2021-01-20 21:57:38 +0100

    Update Notebook metadata

--- src/tohtml.sh
+++ src/tohtml.sh
@@ -1,5 +1,8 @@
 cd ~/git/nbconvert-blog-template/
-jupyter nbconvert ~/git/sme_god-components/statistics.ipynb --to html --output-dir ~/git/sme_god-components_gh-pages --output index.html --template blog
+jupyter nbconvert ~/git/sme_god-components/statistics.ipynb \
+    --to html --output-dir ~/git/sme_god-components_gh-pages \
+    --output index.html \
+    --template blog
 cd ~/git/sme_god-components_gh-pages
 git add .
 git commit -m "Update web version of Notebook at $( date +%T )"
